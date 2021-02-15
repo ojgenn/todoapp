1a6c45046d07d0fabb5fd90bbda0962b11f83f26
udalov.e <udalov.e@n-t.io>
2021-02-15 14:57:02 +0300

    fix callable pipe

--- src/app/pages/home/task-list/show-task/pipes/callable.pipe.ts
+++ src/app/pages/home/task-list/show-task/pipes/callable.pipe.ts
@@ -6,12 +6,17 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class CallablePipe implements PipeTransform {
 
     transform(value: string): string {
-        const reg: RegExp = /(\+7|8)[- _]*\(?[- _]*(\d{3}[- _]*\)?([- _]*\d){7}|\d\d[- _]*\d\d[- _]*\)?([- _]*\d){6})/g;
+        if (value) {
+            const reg: RegExp = /(\+7|8)[- _]*\(?[- _]*(\d{3}[- _]*\)?([- _]*\d){7}|\d\d[- _]*\d\d[- _]*\)?([- _]*\d){6})/g;
 
-        const matches: string[] = value.match(reg);
-        Array.from(matches).forEach((match: string) => {
-            value = value.replace(match, `<a href="tel:${match}">${match}</a>`);
-        });
+            const matches: string[] = value.match(reg);
+
+            if (matches) {
+                Array.from(matches).forEach((match: string) => {
+                    value = value.replace(match, `<a href="tel:${match}">${match}</a>`);
+                });
+            }
+        }
 
         return value;
     }
